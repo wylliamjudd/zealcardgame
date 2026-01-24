46033cd14a5d5ae9506576a032a70be6104929ff
Wylliam Judd <wylliamjudd@gmail.com>
2026-01-23 21:48:39 -0800

    adjust about section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -17,20 +17,17 @@ export default function AboutSection({ email }: Props) {
       <div className="about-section">
         <h1>What is Zeal?</h1>
         <p>
-          Zeal is a strategy card game for 2-8 players, coming soon to
-          Kickstarter. In Zeal you recruit characters, deploy devices and cast
-          spells. There are many ways to play from building your own deck to
-          playing in a draft tournament for up to 8 players.
-        </p>
-        <p>
-          The game plays like a TCG, but without the booster packs. Every card
-          comes in one box.
+          Zeal is a strategy card game for 2-8 players. In Zeal you recruit
+          characters, deploy devices and cast spells. There are many ways to
+          play from building your own deck to playing in a draft tournament for
+          up to 8 players. The game plays like a TCG, but without the booster
+          packs. Every card comes in one box.
         </p>
         <p>
           Sign up to follow the project, and for $1 you can get a print & play
           version and play now.
         </p>
-        <h2>Kickstarter coming soon...</h2>
+        <h2>Coming soon to Kickstarter...</h2>
         <h2>Print & Play available now!</h2>
       </div>
       <button className="scroll-button" onClick={scrollDown}>
