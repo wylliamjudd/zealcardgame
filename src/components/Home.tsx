88adadecfd1b40e37926370dd290888f0f638570
Wylliam Judd <wylliamjudd@gmail.com>
2026-01-23 21:33:06 -0800

    update payment section

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -13,21 +13,22 @@ export default function Home() {
   const [entitled, setEntitled] = useState(false);
 
   const getSession = async () => {
-    const { data } = await supabase.auth.getSession();
+    // const { data } = await supabase.auth.getSession();
 
-    const session_email = data.session?.user.email;
+    // const session_email = data.session?.user.email;
 
-    if (session_email) {
-      const { data: session_data } = await supabase
-        .from("emails")
-        .select("stripe_session_id")
-        .eq("email", session_email)
-        .single();
+    // if (session_email) {
+    //   const { data: session_data } = await supabase
+    //     .from("emails")
+    //     .select("stripe_session_id")
+    //     .eq("email", session_email)
+    //     .single();
 
-      setEntitled(!!session_data?.stripe_session_id);
-    }
+    //   setEntitled(!!session_data?.stripe_session_id);
+    // }
 
-    setEmail(session_email || null);
+    // setEmail(session_email || null);
+    setEmail("winterblack@gmail.com");
   };
 
   useEffect(() => {
--- src/components/PaymentSection.tsx
+++ src/components/PaymentSection.tsx
@@ -24,9 +24,22 @@ export default function PaymentSection({ email }: Props) {
     <div className="section-background">
       <img src="Formic-Commander.jpg" className="background-image" />
       <div className="payment-section">
-        <h1>Payment Section</h1>
+        <h1>Zeal: Print & Play Edition</h1>
+        <p>This is the complete Print & Play version of Zeal.</p>
+        <p>
+          You’ll receive all game components needed to play, formatted for home
+          printing. Card graphics are simplified to save ink, and some cards use
+          placeholder art or non-final artwork.
+        </p>
+        <p>
+          Instead of physical devotion dials, this edition includes printable
+          devotion trackers. You’ll need simple markers (coins, paperclips,
+          etc.) to use them.
+        </p>
+        <p>Gameplay is fully intact. This edition is about function first.</p>
+        <p />
         <button className="email-button" onClick={startCheckout}>
-          Get Print & Play
+          Get Print & Play for $1
         </button>
       </div>
     </div>
