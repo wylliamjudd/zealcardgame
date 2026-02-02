324244bd960bdbfd7764f3b688d6f1e91f134a32
Wylliam Judd <wylliamjudd@gmail.com>
2026-02-02 15:17:26 -0800

    clean slate, remove old site

--- src/components/HeroSection.tsx
+++
@@ -1,8 +0,0 @@
-export default function HeroSection() {
-  return (
-    <div className="hero-section">
-      <img src="/zeal-background.jpg" className="background-image" />
-      <img src="/zeal-logo.png" className="background-image fade-up" />
-    </div>
-  );
-}
